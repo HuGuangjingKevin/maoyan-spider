9a765a759ab72a82e9c26ee43ef8da75e8d71519
huguangjing <huguangjing@globalegrow.com>
2019-05-08 18:16:06 +0800

    新增电影详情其他信息

--- maoyan/maoyan/spiders/maoyanmovie.py
+++ maoyan/maoyan/spiders/maoyanmovie.py
@@ -32,34 +32,55 @@ class MaoyanmovieSpider(scrapy.Spider):
 
     def parse(self, response):
         # 获取主页下面所有的电影详情链接
-        detailHrefList = response.xpath('//dd//a/@href').extract()
+        detailHrefList = response.xpath('//div[@class="movie-item"]/a/@href').extract()
         logging.info("主页:{}下面有{}个电影".format(response.url, len(detailHrefList)))
         for href in detailHrefList:
             # 电影详情页的url
             detailUrl = host + href
-            logging.info("开始请求主页:{}下面的电影详情页:{}".format(response.url, detailUrl))
+            logging.info("开始请求主页下面的电影详情页:{}".format(detailUrl))
             yield scrapy.Request(url=detailUrl, callback=self.parseMovieDetail)
 
     def parseMovieDetail(self, response):
-        font = downLoadWoff(response)
+        item = {}
         # 评分  票房节点
         movieStatsNode = response.xpath('//div[@class="movie-stats-container"]')[0]
-        # 处理电影的用户评分，评分人数，累计票房 都是字体加密后的数据，后面需要进行解密
-        score = movieStatsNode.xpath("./div[1]/div/span/span/text()").extract_first()
-        num = movieStatsNode.xpath("./div[1]/div/div/span/span/text()").extract_first()
-        booking = "".join(movieStatsNode.xpath("./div[2]/div//span/text()").extract())
-        a = re.findall(r'<span class="stonefont">(.*?)</span>', response.text)
-        for i in a:
-            x = getFontNumber(font, i)
-            print(x)
+        # 判断这部电影是否有评分信息
+        movieIdexText = movieStatsNode.xpath("./div[1]/p/text()").extract_first()
+        if movieIdexText != "用户评分":
+            logging.error("{}不存在评分信息，存在的是:{}".format(response.url, movieIdexText))
+            return
+        # 通过正则匹配到电影详情页的.woff地址，请求并保存本地，使用python的FontTools库进行字体文件的处理，得到font对象
+        font = downLoadWoff(response)
+        # 正则提取的字体列表
+        fontTextList = re.findall(r'<span class="stonefont">(.*?)</span>', response.text)
+        if len(fontTextList) == 3:  # 三个信息都有
+            item["score"] = getFontNumber(font, fontTextList[0])
+            item["num"] = getFontNumber(font, fontTextList[1])
+            item["booking"] = getFontNumber(font, fontTextList[2]) + response.xpath(
+                '//span[@class="unit"]/text()').extract_first()
+            item["name"] = response.xpath('//h3[@class="name"]/text()').extract_first()
+            item["nameEllipsis"] = response.xpath('//div[@class="ename ellipsis"]/text()').extract_first()
+            item["type"] = response.xpath('//div[@class="movie-brief-container"]/ul/li[1]/text()').extract_first()
+            countryDuration = response.xpath('//div[@class="movie-brief-container"]/ul/li[2]/text()').extract_first()
+            if "/" in countryDuration:
+                item["country"] = countryDuration.split("/")[0].strip()
+                item["duration"] = countryDuration.split("/")[0].strip()
+            else:
+                item["country"] = countryDuration.strip()
+                item["duration"] = ""
+            item["releaseTime"] = response.xpath(
+                '//div[@class="movie-brief-container"]/ul/li[3]/text()').extract_first()
+            logging.info(item)
+        else:
+            return
 
 
 def downLoadWoff(response):
     # 正则提取woff文件
     woff = regexWoff.search(response.text).group(1)
-    # woff的地址
+    # 前面增加http:，得到woff链接地址
     woffLink = "http:" + woff
-    # 需要将woff文件请求并存入到本地目录中
+    # 请求woff链接后将文件保存到font目录下面
     woffFileName = "font\\" + os.path.basename(woff)
     if not os.path.exists(woffFileName):
         with open(woffFileName, "wb") as file:
@@ -71,14 +92,25 @@ def downLoadWoff(response):
 
 
 def getFontNumber(font, text):
-    # 从字体中提取出来的部分长度4的字符串列表
+    """
+    :param font: 使用python的FontTools库进行字体文件的处理后的font
+    :param text: 现在在html中的字体文本
+    :return: 将字体文本用数字字符串代理后的结果返回
+    """
+    # 从字体中提取出来的部分长度4的字符串列表  数字和字符串
     textExtractList = regexFont.findall(text)
     for textExtract in textExtractList:
-        text = text.replace("&#x{};".format(textExtract), getNum(font, "uni{}".format(textExtract.upper())))
+        numStr = getNum(font, "uni{}".format(textExtract.upper()))
+        text = text.replace("&#x{};".format(textExtract), numStr)
     return text
 
 
 def getNum(font, fonName):
+    """
+    :param font:  FontTools将.woff文件转换成的font
+    :param fonName: 电影详情页的字体名字，以uni开头
+    :return:
+    """
     uniFont = font["glyf"][fonName]
     for key, value in woffDict.items():
         if uniFont == baseFont["glyf"][key]:
